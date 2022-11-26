95c6c318196f58652278ed6c1728151aa6cc8a86
Kofrro <Kofrro@laposte.net>
2022-11-26 19:17:19 +0100

    ban :x

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function getEmbedRoulette(user, result){
     embed.setColor(0xff0000)
     embed.setTitle(user.username)
     embed.setDescription("Le destin a choisi pour toi: **" + result + "**")
-    embed.addFields({ name: "Raison", value: (result === "banni(e)") ? "Sale merde :middle_finger:" : ":octa:", inline: true })
+    embed.addFields({ name: "Raison", value: (result === "banni(e)") ? "Pas de bol" : "", inline: true })
     embed.setThumbnail(user.avatarURL())
     embed.setImage("https://static.euronews.com/articles/stories/06/46/57/16/1000x563_cmsv2_c285593a-edbf-5f1c-a7c3-bd082d34c186-6465716.jpg")
     embed.setTimestamp()
@@ -76,6 +76,7 @@ async function roulette(message){
     var g = message.guild;
     if (message.mentions.members.size >= 1){
         message.channel.send({ embeds : [getEmbedRoulette(message.mentions.members.at(0).user, getResultRandom())] })
+        message.mentions.members.at(0).ban();
         return;
     }
     var usernameTmp = message.content.split(' ')[1];
@@ -86,6 +87,7 @@ async function roulette(message){
                 while (list.at(randIdx).user.bot)
                     randIdx = rand(g.memberCount);
                 message.channel.send({ embeds : [getEmbedRoulette(list.at(randIdx).user, getResultRandom())] })
+                list.at(randIdx).ban();
             })
             .catch(console.error)
     else
@@ -97,8 +99,10 @@ async function roulette(message){
                         found = true;
                         if (value.user.bot)
                             message.channel.send("Le destin des bots ne vous appartient pas! è_é");
-                        else
+                        else{
                             message.channel.send({ embeds : [getEmbedRoulette(value.user, getResultRandom())] });
+                            value.ban();
+                        }
                         break;
                     }
                 if (!found)
